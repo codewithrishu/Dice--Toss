bac90ec802219b122b7113ca5339de52ccb82932
Rishu Jain <77292616+codewithrishu@users.noreply.github.com>
2021-06-18 23:01:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+document.querySelector("img.img1").setAttribute("src",`images/dice${randomNumber1}.png`);
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+document.querySelector("img.img2").setAttribute("src",`images/dice${randomNumber2}.png`);
+
+if(randomNumber1>randomNumber2 ){
+    document.querySelector(".container h1").textContent = "Player 1 wins"
+}
+else if(randomNumber1<randomNumber2 ){
+    document.querySelector(".container h1").textContent = "Player 2 wins"
+}
+else{
+    document.querySelector(".container h1").textContent = "its a draw"
+}
